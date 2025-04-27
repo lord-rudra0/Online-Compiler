004c28cde7e07ad61e2db7ca86bba48a83a72996
Rudra Pratap Singh <rudrapratapsingh2026@cs.ajce.in>
2025-04-27 23:40:07 +0530

    theme of editor added

--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from '@monaco-editor/react';
 import "./Editor.css";
+import { FaSun, FaMoon } from 'react-icons/fa';
 
-const EditorBox = () => {
+
+
+function EditorBox() {
+  const [theme, setTheme] = useState('vs-dark'); 
+
+  function themeChange() {
+    // Toggle between vs-dark and light
+    setTheme(prevTheme => (prevTheme === 'vs-dark' ? 'light' : 'vs-dark'));
+  }
+
   return (
-    <div 
-      className="editor" 
-    //   style={{ height: '500px', backgroundColor: 'rgb(233, 61, 61)' }}
-    >
+    <div>
+        <div>
+      <button onClick={themeChange} style={{ fontSize: '24px', background: 'none', border: 'none', cursor: 'pointer' }}>
+        {theme === 'vs-dark' ? <FaSun /> : <FaMoon />}
+      </button>
+
       <Editor
+        className="editor"
         height="500px"
-        width="500px"
+        width="500px"     
         language="javascript"
         value="// Hello World"
-        theme="vs-dark"
+        theme={theme}    // set the theme here
         options={{
           fontSize: 16,
           minimap: { enabled: false },
@@ -21,7 +34,8 @@ const EditorBox = () => {
         }}
       />
     </div>
+    </div>
   );
-};
+}
 
 export default EditorBox;
